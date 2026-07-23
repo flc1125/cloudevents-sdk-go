c628b909178bb5de9e753185bc0512d376c52b08
Doug Davis <duglin@gmail.com>
2026-07-23 15:37:51 +0000

    remove bad comment
    Signed-off-by: Doug Davis <duglin@gmail.com>

--- v2/protocol/http/message_test.go
+++ v2/protocol/http/message_test.go
@@ -158,7 +158,6 @@ func TestMessageTransformDeleteExtension(t *testing.T) {
 // TestReadBinaryEmptyCeHeaderNoPanic verifies that a header whose name is
 // exactly the "Ce-" prefix (i.e. no attribute/extension name follows it)
 // does not cause ReadBinary to panic with an index-out-of-range error.
-// See advisory-01-binary-header-panic-dos.md.
 func TestReadBinaryEmptyCeHeaderNoPanic(t *testing.T) {
 	h := http.Header{}
 	h.Set("Ce-Specversion", "1.0")
